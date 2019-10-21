cf3bbad2e7f86bc9619d4e9b7b5669edfdbff9bb
julian <julianpitney@gmail.com>
2019-10-21 16:15:02 -0400

    Finished implementing 3D density color map projection

--- src/main.py
+++ src/main.py
@@ -41,10 +41,15 @@ def compare_stacks(original, processed, auto):
 
 def max_project(stack):
 
-    cv2.namedWindow('Max Projection', cv2.WINDOW_NORMAL)
     stackMax = np.max(stack, axis=0)
-    cv2.imshow('Max Projection', stackMax)
-    cv2.waitKey(0)
+    return stackMax
+
+def min_project(stack):
+
+    stackMin = np.min(stack, axis=0)
+    return stackMin
+
+
 
 
 def save_stack(stack):
@@ -58,19 +63,27 @@ def save_stack(stack):
 # (either manual inspection or automatic)
 def remove_all_pixels_below_threshold(stack, threshold):
 
+    print("remove_all_pixels_below_threshold(): Starting...")
+
     for z in range(0, len(stack)):
         ret, thresholded_slice = cv2.threshold(stack[z], threshold, 255, cv2.THRESH_TOZERO)
         stack[z] = thresholded_slice
 
+    print("remove_all_pixels_below_threshold(): Completed!")
+
 def remove_all_pixels_above_threshold(stack, threshold):
+
+    print("remove_all_pixels_above_threshold(): Starting...")
 
     for z in range(0, len(stack)):
         ret, thresholded_slice = cv2.threshold((stack[z]), threshold, 255, cv2.THRESH_TOZERO_INV)
         stack[z] = thresholded_slice
 
-
+    print("remove_all_pixels_above_threshold(): Completed!")
 
 def kernel_filter_2d(stack, kernelDims):
+
+    print("kernel_filter_2d(): Starting...")
 
     kernel = np.ones(kernelDims, np.float32) / (kernelDims[0] * kernelDims[1])
 
@@ -78,10 +91,28 @@ def kernel_filter_2d(stack, kernelDims):
 
         stack[z] = cv2.filter2D(stack[z], -1, kernel)
 
+    print("kernel_filter_2d(): Completed!")
+
+def map_actual_range_to_max_range(val):
+
+    leftMin = 0
+    leftMax = 40
+    leftRange = leftMax - leftMin
+    rightMin = 0
+    rightMax = 255
+    rightRange = rightMax - rightMin
+
+    if val > leftMax:
+        val = leftMax
+
+    scaled = float(val - leftMin) / float(leftRange)
+    return math.floor(rightMin + (scaled * rightRange))
 
 
 
 def gen_signal_density_map_3d(stack, kernelSize_zyx):
+
+    print("gen_signal_density_map_3d(): Starting...")
 
     zLen = stack.shape[0]
     yLen = stack.shape[1]
@@ -106,6 +137,7 @@ def gen_signal_density_map_3d(stack, kernelSize_zyx):
 
     for z in range(0, zBlocks):
 
+
         for y in range(0, yBlocks):
 
             for x in range(0, xBlocks):
@@ -118,46 +150,69 @@ def gen_signal_density_map_3d(stack, kernelSize_zyx):
                     totalVolume += slice.size
 
                 density = totalSignal / totalVolume
-                stack[zBounds[0]:zBounds[1], yBounds[0]:yBounds[1], xBounds[0]:xBounds[1]] = int(density*255)
-
-
+                stack[zBounds[0]:zBounds[1], yBounds[0]:yBounds[1], xBounds[0]:xBounds[1]] = map_actual_range_to_max_range(int(density*255))
 
                 xBounds[0] += kernelSize_zyx[2]
                 xBounds[1] += kernelSize_zyx[2]
                 if xPixelsRemainder != 0 and x == xBlocks - 1:
-                    print("X BOUNDARY DETECTED")
+                    # TODO: Implement boundary detection case
                     break
 
             xBounds = [0, kernelSize_zyx[2]]
             yBounds[0] += kernelSize_zyx[1]
             yBounds[1] += kernelSize_zyx[1]
             if yPixelsRemainder != 0 and y == yBlocks - 1:
-                print("Y BOUNDARY DETECTED")
+                # TODO: Implement boundary detection case
                 break
 
         yBounds = [0, kernelSize_zyx[1]]
         zBounds[0] += kernelSize_zyx[0]
         zBounds[1] += kernelSize_zyx[0]
         if zPixelsRemainder != 0 and z == zBlocks - 1:
-            print("Z BOUNDARY DETECTED")
+            #TODO: Implement boundary detection case
             break
 
+    print("gen_signal_density_map_3d(): Completed!")
 
 def convert_density_map_to_color(stack):
 
     color_stack = np.stack((stack,)*3, axis=-1)
-    color_stack[:, :, :, 0:2] = 0
-
     return color_stack
 
-plt.hist(stack.ravel(), 256, [0, 256])
+
+def color_map(stackGray, stackColor):
+
+
+    for z in range(0, len(stackGray)):
+
+        stackColor[z] = cv2.applyColorMap(stackGray[z], 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#plt.hist(stack.ravel(), 256, [0, 256])
 remove_all_pixels_below_threshold(stack, 24)
-kernel_filter_2d(stack, (2, 2))
-gen_signal_density_map_3d(stack, (10, 30, 30))
-color_stack = convert_density_map_to_color(stack)
-#max_project(color_stack)
-save_stack(color_stack)
+kernel_filter_2d(stack, (3, 3))
+gen_signal_density_map_3d(stack, (5, 10, 10))
+maxProjection = max_project(stack)
 
-
+maxProjection = cv2.GaussianBlur(maxProjection, (101, 101), 0)
+maxProjectionColor = convert_density_map_to_color(maxProjection)
+maxProjectionColorMapped = cv2.applyColorMap(maxProjectionColor, 2)
+cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+cv2.imshow('test', maxProjectionColorMapped)
+cv2.waitKey(0)
+cv2.imwrite('density_map.png', maxProjectionColorMapped)
 
 
